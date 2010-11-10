fa7188842b140688614010b34b68c64c6e2d7ec4
David Glick <dglick@gmail.com>
2010-11-10 22:35:25 +0000

    apply jessesnyder's workaround to provide a default location (this needs a better fix so that we can have non-required fields)
    git-svn-id: svn://svn.plone.org/plone/plone.formwidget.geolocation/trunk@45750 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- plone/formwidget/geolocation/widget.py
+++ plone/formwidget/geolocation/widget.py
@@ -1,5 +1,5 @@
 from zope.interface import implementer, implementsOnly
-from zope.component import adapter
+from zope.component import adapter, getMultiAdapter
 
 from z3c.form.interfaces import IFieldWidget, IFormLayer
 from z3c.form.widget import FieldWidget
@@ -7,11 +7,23 @@ from z3c.form.browser.text import TextWidget
 
 from plone.formwidget.geolocation.interfaces import IGeolocationField, IGeolocationWidget
 
+
 class GeolocationWidget(TextWidget):
     implementsOnly(IGeolocationWidget)
 
     klass = u'geolocation-widget'
-    value = None # don't default to a string
+    value = None
+    
+    def update(self):
+        super(GeolocationWidget, self).update()
+        if self.value is None:
+            self.value = self._default_loc()
+    
+    def _default_loc(self):
+        config = getMultiAdapter((self.context, self.request), 
+                                    name="maps_configuration")
+        return tuple(config.default_location)
+
 
 @implementer(IFieldWidget)
 @adapter(IGeolocationField, IFormLayer)
