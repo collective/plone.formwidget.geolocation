f10993d8e776cf2faedb1c5cb989b2a0f30b16dc
David Glick <david@glicksoftware.com>
2013-05-23 14:51:57 -0700

    add supermodel handler, fix __all__

--- plone/formwidget/geolocation/__init__.py
+++ plone/formwidget/geolocation/__init__.py
@@ -4,4 +4,4 @@ _ = MessageFactory('plone.formwidget.geolocation')
 from plone.formwidget.geolocation.geolocation import Geolocation
 from plone.formwidget.geolocation.field import GeolocationField
 
-__all__ = (_, Geolocation, GeolocationField)
+__all__ = ('_', 'Geolocation', 'GeolocationField')
