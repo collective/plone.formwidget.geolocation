7318fb474fcd817d421cf3aa0f27e284a1db078f
Benoit Suttor <bsuttor@imio.be>
2020-01-27 16:09:30 +0100

    Add maps_configuration view

---
+++ plone/formwidget/geolocation/view.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# from collective.realestate import _
+from plone import api
+from Products.Five.browser import BrowserView
+
+
+class MapsConfiguration(BrowserView):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    @property
+    def default_location(self):
+        key_lat = "geolocation.default_latitude"
+        key_lon = "geolocation.default_longitude"
+        lat = api.portal.get_registry_record(key_lat, default=0.0)
+        lon = api.portal.get_registry_record(key_lon, default=0.0)
+        return (lat, lon)
