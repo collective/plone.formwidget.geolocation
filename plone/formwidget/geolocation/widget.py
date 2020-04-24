fba98ab9a8d45650ba9a966c76d50213afe3615e
Benoit Suttor <bsuttor@imio.be>
2020-04-24 13:42:13 +0200

    Become black compliant

--- plone/formwidget/geolocation/widget.py
+++ plone/formwidget/geolocation/widget.py
@@ -23,29 +23,29 @@ import six
 @implementer_only(IGeolocationWidget)
 class GeolocationWidget(TextWidget):
 
-    klass = u'geolocation-widget'
+    klass = u"geolocation-widget"
     value = None
 
     def __init__(self, request):
         top_request = get_top_request(request)
         # Just add the bundle from plone.patternslib.
         # If it's not available, it wont't hurt.
-        add_bundle_on_request(top_request, 'bundle-leaflet')
+        add_bundle_on_request(top_request, "bundle-leaflet")
 
         super(GeolocationWidget, self).__init__(request)
 
     def update(self):
         super(GeolocationWidget, self).update()
-        if self.value is None and self.mode == 'input':
+        if self.value is None and self.mode == "input":
             self.value = self._default_loc()
 
     @property
     def id_input_lat(self):
-        return u'{0}_latitude'.format(self.id)
+        return u"{0}_latitude".format(self.id)
 
     @property
     def id_input_lng(self):
-        return u'{0}_longitude'.format(self.id)
+        return u"{0}_longitude".format(self.id)
 
     @property
     def data_geojson(self):
@@ -55,71 +55,69 @@ class GeolocationWidget(TextWidget):
         if not coordinates:
             return
 
-        title = getattr(self.context, 'title', '')
-        description = getattr(self.context, 'description', '')
+        title = getattr(self.context, "title", "")
+        description = getattr(self.context, "description", "")
 
         geo_json = {
-            'type': 'FeatureCollection',
-            'features': [
+            "type": "FeatureCollection",
+            "features": [
                 {
-                    'type': 'Feature',
-                    'properties': {
-                        'popup': u'<h3>{0}</h3><p>{1}</p>'.format(
-                            safe_unicode(title),
-                            safe_unicode(description)
+                    "type": "Feature",
+                    "properties": {
+                        "popup": u"<h3>{0}</h3><p>{1}</p>".format(
+                            safe_unicode(title), safe_unicode(description)
                         )
                     },
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [
-                            coordinates[1],  # lng
-                            coordinates[0]   # lat
-                        ]
-                    }
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [coordinates[1], coordinates[0]],  # lng  # lat
+                    },
                 },
-            ]
+            ],
         }
 
-        if self.mode == 'input':
-            properties = geo_json['features'][0]['properties']
-            properties['editable'] = True
-            properties['no_delete'] = True
-            properties['latinput'] = u'#{0}'.format(self.id_input_lat)
-            properties['lnginput'] = u'#{0}'.format(self.id_input_lng)
+        if self.mode == "input":
+            properties = geo_json["features"][0]["properties"]
+            properties["editable"] = True
+            properties["no_delete"] = True
+            properties["latinput"] = u"#{0}".format(self.id_input_lat)
+            properties["lnginput"] = u"#{0}".format(self.id_input_lng)
 
         return json.dumps(geo_json)
 
     @property
     def map_configuration(self):
-        map_layers = getrec('geolocation.map_layers') or []
+        map_layers = getrec("geolocation.map_layers") or []
         config = {
-            "fullscreencontrol": getrec('geolocation.fullscreen_control'),
-            "locatecontrol": getrec('geolocation.locate_control'),
-            "zoomcontrol": getrec('geolocation.zoom_control'),
-            "minimap": getrec('geolocation.show_minimap'),
-            "addmarker": getrec('geolocation.show_add_marker'),
-            "geosearch": getrec('geolocation.show_geosearch'),
-            "geosearch_provider": getrec('geolocation.geosearch_provider'),
-            "default_map_layer": getrec('geolocation.default_map_layer'),
+            "fullscreencontrol": getrec("geolocation.fullscreen_control"),
+            "locatecontrol": getrec("geolocation.locate_control"),
+            "zoomcontrol": getrec("geolocation.zoom_control"),
+            "minimap": getrec("geolocation.show_minimap"),
+            "addmarker": getrec("geolocation.show_add_marker"),
+            "geosearch": getrec("geolocation.show_geosearch"),
+            "geosearch_provider": getrec("geolocation.geosearch_provider"),
+            "default_map_layer": getrec("geolocation.default_map_layer"),
             "map_layers": [
                 {"title": translate(_(l), context=self.request), "id": l}
-                for l in map_layers],
+                for l in map_layers
+            ],
         }
-        if self.mode == 'input':
+        if self.mode == "input":
             # geosearch for input is always active
-            config['geosearch'] = True
+            config["geosearch"] = True
             # zoomcontrol for input is always active
-            config['zoomcontrol'] = True
+            config["zoomcontrol"] = True
         return json.dumps(config)
 
     def _default_loc(self):
-        config = queryMultiAdapter((self.context, self.request),
-                                   name="maps_configuration", default=None)
+        config = queryMultiAdapter(
+            (self.context, self.request), name="maps_configuration", default=None
+        )
         default = ret = (0.0, 0.0)
-        if config and hasattr(config, 'default_location'):
+        if config and hasattr(config, "default_location"):
             ret = config.default_location
             if isinstance(ret, six.string_types):
-                ret = ret.split(',')
+                ret = ret.split(",")
         if len(ret) != 2:
             return default
         return (float(ret[0]), float(ret[1]))
