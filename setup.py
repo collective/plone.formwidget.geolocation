67c65a4597a43d87fcb3e2f6a116f03b984baf6c
Jens W. Klein <jk@kleinundpartner.at>
2020-07-10 18:24:18 +0200

    metadata

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
     keywords='',
     author='David Glick',
