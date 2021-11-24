c6c3187b664deed93394c2455c70b392239989e1
yurj <yuri.carrer@unipd.it>
2021-11-24 10:25:25 +0100

    add a basic deserializer

--- plone/formwidget/geolocation/serializer.py
+++ plone/formwidget/geolocation/serializer.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from plone.dexterity.interfaces import IDexterityContent
 from plone.formwidget.geolocation.interfaces import IGeolocationField
+from plone.formwidget.geolocation import Geolocation
 from plone.restapi.interfaces import IFieldSerializer
+from plone.restapi.interfaces import IFieldDeserializer
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.serializer.dxfields import DefaultFieldSerializer
+from plone.restapi.deserializer.dxfields import DefaultFieldDeserializer
 from zope.component import adapter
 from zope.interface import Interface
 from zope.interface import implementer
@@ -20,3 +23,9 @@ class GeolocationSerializer(DefaultFieldSerializer):
             geolocation["longitude"] = value.longitude
 
         return json_compatible(geolocation)
+
+@adapter(IGeolocationField, IDexterityContent, Interface)
+@implementer(IFieldDeserializer)
+class GeolocationDeserializer(DefaultFieldDeserializer):
+    def __call__(self, value):
+        return Geolocation(value["latitude"] , value["longitude"])
