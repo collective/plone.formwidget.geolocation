3f46fbb477bf4e2ba589401730a32aae34191d4b
Christophe Boulanger <christophe.boulanger@imio.be>
2022-05-25 15:08:21 +0200

    Add some tests

---
+++ plone/formwidget/geolocation/tests/test_serializer.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from plone.formwidget.geolocation.testing import GEOLOCATION_INTEGRATION_TESTING
+from plone.dexterity.content import DexterityContent
+from plone.dexterity.utils import iterSchemata
+from plone.formwidget.geolocation.field import GeolocationField
+from plone.formwidget.geolocation.geolocation import Geolocation
+from plone.restapi.interfaces import IFieldSerializer
+from plone.supermodel import model
+from zope.component import getMultiAdapter
+from zope.interface import implementer
+
+import unittest2 as unittest
+
+
+class IDummyGeolocation(model.Schema):
+    """"""
+
+    geolocation = GeolocationField(title="Geolocation")
+
+
+@implementer(IDummyGeolocation)
+class DummyContent(DexterityContent):
+    """"""
+
+
+class TestSerializer(unittest.TestCase):
+
+    layer = GEOLOCATION_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+
+    def test_serializer(self):
+        content = DummyContent()
+
+        content.geolocation = None
+        field = IDummyGeolocation.get("geolocation")
+        serializer = getMultiAdapter((field, content, self.request), IFieldSerializer)
+        result = serializer()
+        self.assertEqual(result, {})
+
+        content.geolocation = Geolocation()
+        field = IDummyGeolocation.get("geolocation")
+        serializer = getMultiAdapter((field, content, self.request), IFieldSerializer)
+        result = serializer()
+        self.assertEqual(result, {"latitude": 0.0, "longitude": 0.0})
+
+        content.geolocation = Geolocation(50.0, 5.0)
+        serializer = getMultiAdapter((field, content, self.request), IFieldSerializer)
+        result = serializer()
+        self.assertEqual(result, {"latitude": 50.0, "longitude": 5.0})
---
+++ plone/formwidget/geolocation/tests/test_widget.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+from plone.api.portal import set_registry_record
+from plone.formwidget.geolocation.interfaces import IGeolocationWidget
+from plone.formwidget.geolocation.testing import GEOLOCATION_INTEGRATION_TESTING
+from plone.formwidget.geolocation.widget import GeolocationWidget
+from z3c.form.widget import WidgetTemplateFactory
+from zope.pagetemplate.interfaces import IPageTemplate
+
+import json
+import os
+import unittest2 as unittest
+import zope.component
+
+
+class TestWidget(unittest.TestCase):
+
+    layer = GEOLOCATION_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+
+    def test_bundle_on_request(self):
+        GeolocationWidget(self.request)
+        bundles = getattr(self.request, "enabled_bundles", [])
+        self.assertListEqual(bundles, ["bundle-leaflet"])
+
+    def test_value(self):
+        widget = GeolocationWidget(self.request)
+        widget.id = widget.name = "geolocation"
+        widget.request = {widget.name: (50.0, 5.0)}
+        widget.update()
+        self.assertEqual(widget.value, (50.0, 5.0))
+
+        widget.request = {widget.name: None}
+        widget.update()
+        self.assertEqual(widget.value, (None, None))
+
+        widget.mode = "input"
+        widget.update()
+        self.assertEqual(widget.value, (None, None))
+
+        set_registry_record("geolocation.default_latitude", 70.0)
+        set_registry_record("geolocation.default_longitude", 7.0)
+        widget.update()
+        self.assertEqual(widget.value, (70.0, 7.0))
+
+    def test_data_geojson(self):
+        widget = GeolocationWidget(self.request)
+        widget.id = widget.name = "geolocation"
+        widget.request = {widget.name: None}
+        widget.update()
+        self.assertEqual(widget.data_geojson, None)
+
+        widget.request = {widget.name: (50.0, None)}
+        widget.update()
+        self.assertEqual(widget.data_geojson, None)
+
+        widget.request = {widget.name: (50.0, 5.0)}
+        widget.update()
+        json_result = json.loads(widget.data_geojson)
+        coordinates = json_result["features"][0]["geometry"]["coordinates"]
+        self.assertEqual(coordinates, [5.0, 50.0])
+
+    def test_default_loc(self):
+        widget = GeolocationWidget(self.request)
+        widget.id = widget.name = "geolocation"
+        widget.request = {widget.name: None}
+
+        widget.update()
+        self.assertEqual(widget._default_loc(), (None, None))
+
+        set_registry_record("geolocation.default_latitude", 70.0)
+        set_registry_record("geolocation.default_longitude", 7.0)
+        widget.update()
+        self.assertEqual(widget._default_loc(), (70.0, 7.0))
+
+    def test_render(self):
+        widget = GeolocationWidget(self.request)
+        widget.id = widget.name = "geolocation"
+        widget.request = {widget.name: (50.0, 5.0)}
+        widget.update()
+        p = os.path.abspath("..")
+        zope.component.provideAdapter(
+            WidgetTemplateFactory("../geolocation_input.pt", "text/html"),
+            (None, None, None, None, IGeolocationWidget),
+            IPageTemplate,
+            name=widget.mode,
+        )
+        widget.render()
