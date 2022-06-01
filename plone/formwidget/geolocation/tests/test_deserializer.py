aa82d33b66ad76ddac793ee28fd1e0069d3430b0
Laurent Lasudry <lasudry@gmail.com>
2022-06-01 12:28:56 +0200

    Add more tests (deserializer, default location, map condition)

---
+++ plone/formwidget/geolocation/tests/test_deserializer.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from plone.formwidget.geolocation.testing import GEOLOCATION_INTEGRATION_TESTING
+from plone.formwidget.geolocation.interfaces import IGeolocation
+from plone.formwidget.geolocation.tests.utils import DummyContent
+from plone.formwidget.geolocation.tests.utils import IDummyGeolocation
+from plone.restapi.interfaces import IFieldDeserializer
+from zope.component import getMultiAdapter
+
+import unittest2 as unittest
+
+
+class TestDeserializer(unittest.TestCase):
+
+    layer = GEOLOCATION_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.request = self.layer["request"]
+
+    def test_deserializer(self):
+        content = DummyContent()
+
+        content.geolocation = None
+        field = IDummyGeolocation.get("geolocation")
+        deserializer = getMultiAdapter(
+            (field, content, self.request), IFieldDeserializer
+        )
+        with self.assertRaises(ValueError):
+            deserializer(None)
+        with self.assertRaises(ValueError):
+            deserializer([])
+        with self.assertRaises(ValueError):
+            deserializer({})
+
+        result = deserializer({"latitude": 0.0, "longitude": 0.0})
+        self.assertTrue(IGeolocation.providedBy(result))
+        self.assertEqual(result.latitude, 0.0)
+        self.assertEqual(result.longitude, 0.0)
+
+        result = deserializer({"latitude": 50.0, "longitude": 5.0})
+        self.assertEqual(result.latitude, 50.0)
+        self.assertEqual(result.longitude, 5.0)
